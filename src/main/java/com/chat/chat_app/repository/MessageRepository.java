6690c668e760b074cecfd25e6a405db58df43ae7
unknown <kukrejauday@gmail.com>
2026-03-20 11:53:21 +0530

    All services done - UserService, ChatRoomService, MessageService

--- src/main/java/com/chat/chat_app/repository/MessageRepository.java
+++ src/main/java/com/chat/chat_app/repository/MessageRepository.java
@@ -1,10 +1,18 @@
 package com.chat.chat_app.repository;
 
+import com.chat.chat_app.entity.ChatRoom;
 import com.chat.chat_app.entity.Message;
+import com.chat.chat_app.entity.User;
 import org.hibernate.boot.models.JpaAnnotations;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MessageRepository extends JpaRepository<Message,Long> {
+    List<Message> findByChatRoom(ChatRoom chatRoom);
+    List<Message> findBySenderAndReceiver(User sender, User receiver);
+
+
 }
---
+++ src/main/java/com/chat/chat_app/service/MessageService.java
@@ -0,0 +1,63 @@
+package com.chat.chat_app.service;
+
+import com.chat.chat_app.entity.ChatRoom;
+import com.chat.chat_app.entity.Message;
+import com.chat.chat_app.entity.User;
+import com.chat.chat_app.repository.ChatRoomRepository;
+import com.chat.chat_app.repository.MessageRepository;
+import com.chat.chat_app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.MessagingAdviceBean;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class MessageService {
+    @Autowired
+    ChatRoomRepository chatRoomRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    MessageRepository messageRepository;
+
+    public Message sendMessage(long senderid,long recieverid ,String content){
+        Message message1=new Message();
+        message1.setSender(userRepository.getReferenceById(senderid));
+        message1.setReceiver(userRepository.getReferenceById(recieverid));
+        message1.setContent(content);
+        message1.setTimeStamp(new Date());
+        messageRepository.save(message1);
+        return message1;
+    }
+    public Message sendGroupMessage(long senderid,long chatroomId ,String content){
+        Message message1=new Message();
+        message1.setSender(userRepository.getReferenceById(senderid));
+        message1.setChatRoom(chatRoomRepository.getReferenceById(chatroomId));
+        message1.setContent(content);
+        message1.setTimeStamp(new Date());
+        messageRepository.save(message1);
+        return message1;
+    }
+    public List<Message> getMessagesByChatRoom(long chatRoomId){
+        ChatRoom chatRoom=chatRoomRepository.getReferenceById(chatRoomId);
+        List<Message> messages=messageRepository.findByChatRoom(chatRoom);
+       return messages;
+    }
+    public List<Message> getPrivateMessages(long senderId, long receiverId){
+        User sender=userRepository.getReferenceById(senderId);
+        User receiver=userRepository.getReferenceById(receiverId);
+        List<Message> messages=messageRepository.findBySenderAndReceiver(sender,receiver);
+        return messages;
+    }
+    public List<User> ReadBy(long messageId,long userId){
+        Message message=messageRepository.getReferenceById(messageId);
+        User user =userRepository.getReferenceById(userId);
+        List<User> readBy = message.getReadBy();
+        readBy.add(user);
+        message.setReadBy(readBy);
+        messageRepository.save(message);
+        return readBy;
+    }
+}
